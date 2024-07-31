415dd77e8a49cfd11fc7692fbc62f8aa4c94c4c6
Vinicius Melchior <viniciusmelchior.vmls@gmail.com>
2024-07-31 10:32:49 -0300

    Add docs

--- RoomsProject/settings.py
+++ RoomsProject/settings.py
@@ -87,5 +87,8 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / "media"
 
+LOGIN_URL = '/login/'
+
+LOGIN_REDIRECT_URL = ''
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
