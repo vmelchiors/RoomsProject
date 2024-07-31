f8056ccc56c3344e668df733b1610251d7fa02a1
Vinicius Melchior <viniciusmelchior.vmls@gmail.com>
2024-07-31 11:01:25 -0300

    Mysql connection

--- RoomsProject/settings.py
+++ RoomsProject/settings.py
@@ -51,8 +51,12 @@ WSGI_APPLICATION = 'RoomsProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Rooms',
+        'USER': 'root',
+        'PASSWORD': '1912',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
